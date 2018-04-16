d74cf3184648fdefde47f05104bca15bc146cf0b
Rodrigues <gabriel.c.rodrigues@gmail.com>
2018-04-16 22:31:32 +0800

    tried examples on Set and Generator

--- goodies.py
+++ goodies.py
@@ -84,3 +84,60 @@ def only_upper(t):
 
 n = 'MaryChong'
 print(only_upper(n))
+
+"""
+Generator Expressions
+
+"""
+g = (x**2 for  x in range(5)) #similar to list but expression in parenthesis instead of bracket
+print(g) # result is a generator object that can iterate through a sequence of values
+
+next(g)
+next(g)
+for val in g:
+    print(val)
+
+next(g) #once generator is exhausted, will raise a StopIteration message
+
+"""
+Sets
+behaves like a collection of Dictionary keys with no values
+
+"""
+d1 = {'a':1, 'b':2, 'c':3}
+d2 = {'a':2, 'd':4}
+
+def substract(d1, d2):
+    return set(d1) - set(d2) # returns a set {'b', 'c'}
+
+def has_duplicates(t):
+    d = {}
+    for x in t:
+        if x in d:
+            return True
+        d[x]= True
+    return False
+
+lst = [2, 3, 4, 3]
+has_duplicates(lst) #returns True
+
+def has_duplicates1(t):
+    return len(set(t)) < len(t)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
