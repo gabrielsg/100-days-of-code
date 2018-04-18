ff39fa1287aeb17ccc4bec587fb709008b15a31c
Rodrigues <gabriel.c.rodrigues@gmail.com>
2018-04-18 23:37:46 +0800

    review examples on Counter, defaultdict, namedtuple, keyword argument

--- goodies.py
+++ goodies.py
@@ -92,12 +92,12 @@ Generator Expressions
 g = (x**2 for  x in range(5)) #similar to list but expression in parenthesis instead of bracket
 print(g) # result is a generator object that can iterate through a sequence of values
 
-next(g)
-next(g)
-for val in g:
-    print(val)
-
-next(g) #once generator is exhausted, will raise a StopIteration message
+#next(g)
+#next(g)
+#for val in g:
+#    print(val)
+#
+#next(g) #once generator is exhausted, will raise a StopIteration message
 
 """
 Sets
@@ -124,20 +124,51 @@ has_duplicates(lst) #returns True
 def has_duplicates1(t):
     return len(set(t)) < len(t)
 
+"""
+Counter is like a set except it keeps track of how many times an element appears
+
+"""
+
+from collections import Counter
+count = Counter('passport')
+for val, freq in count.most_common(3):
+    print(val, freq)
+
+"""
+defaultdict is also from the collections module. It is like a dictionary except
+if you access a key that doesn't exist, it can generate a new value on the fly
+
+"""
+from collections import defaultdict
+d = defaultdict(list) #argument is list which is a class object
+t = d['new key']
+print(t)
+t.append('new value')
+t.append('another value')
+print(d) #{'new key': ['new value', 'another value']}
+
+"""
+Named tuple : used instead of simple classes
+
+"""
+from collections import namedtuple
+Point = namedtuple('Point', ['x', 'y'])
+print(Point)
+p = Point(1, 2)
+print(p)
+print(p.x, p.y)
+x, y = p
+print(x, y)
 
 
+"""
+Gather keyword arguments
+* operator gather positional arguments into a tuple
+** operator gatther keyword arguments
 
+"""
+def printall(*args, **kargs):
+    print(args, kargs)
 
-
-
-
-
-
-
-
-
-
-
-
-
+printall(1, 2, value=5)
 
